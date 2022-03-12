8a81d80e33cf3d4f0cf16fcc0d9738ce9b369bf8
Abhishek Maity <abhishek.maity@flutura.com>
2022-03-12 17:07:53 +0530

    update tower of hanoi problem

--- DSA practice/6-Tower_of_Hanoi.py
+++ DSA practice/6-Tower_of_Hanoi.py
@@ -1,12 +1,11 @@
-def towerofhanoi(n, from_bar, aux_bar, to_bar):
+def towerofhanoi(n, from_bar, to_bar, aux_bar):
     if n ==1:
         print("Move disk 1 from bar ", from_bar, " to bar ", to_bar)
         return
-    towerofhanoi(n-1, from_bar, to_bar, aux_bar)
+    towerofhanoi(n-1, from_bar, aux_bar, to_bar)
     print("Move disk ", n, " from bar ", from_bar, " to_bar ", to_bar)
     towerofhanoi(n-1, aux_bar, to_bar, from_bar)
 
-
 if __name__ == '__main__':
-    n=5
-    towerofhanoi(n, "A", "B", "C")
+    n=10
+    towerofhanoi(n, "A", "C", "B")
