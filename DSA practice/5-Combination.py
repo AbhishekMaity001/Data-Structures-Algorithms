0410d0056f51c7b9f0aab371e3dbf152d13d982d
Abhishek Maity <abhishek.maity97@gmail.com>
2022-03-13 11:56:28 +0530

    n C r Problem

--- DSA practice/5-Combination.py
+++ DSA practice/5-Combination.py
@@ -4,10 +4,13 @@ def fact(n):
     else:
         return n*fact(n-1)
 
+
 def combination(n, r):
-    if n==r:
+    if n == r:
         return 1
-    return (fact(n)/(fact(n-r)*fact(r)))
+    return fact(n)/(fact(n-r)*fact(r))
+
 
 if __name__ == '__main__':
     print(combination(5, 2))
+
