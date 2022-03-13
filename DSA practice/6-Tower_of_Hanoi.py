aa9fcb0e19b31d2bae53a6dc0427411527c8b31d
Abhishek Maity <abhishek.maity97@gmail.com>
2022-03-13 11:56:58 +0530

    Tower of Hanoi problem

--- DSA practice/6-Tower_of_Hanoi.py
+++ DSA practice/6-Tower_of_Hanoi.py
@@ -1,11 +1,17 @@
 def towerofhanoi(n, from_bar, to_bar, aux_bar):
-    if n ==1:
+    """
+    n-1 from A to B using C
+    1 from A to C
+    n-1 from B to C using A
+    """
+    if n == 1:
         print("Move disk 1 from bar ", from_bar, " to bar ", to_bar)
         return
     towerofhanoi(n-1, from_bar, aux_bar, to_bar)
     print("Move disk ", n, " from bar ", from_bar, " to_bar ", to_bar)
     towerofhanoi(n-1, aux_bar, to_bar, from_bar)
 
+
 if __name__ == '__main__':
-    n=10
+    n = 10
     towerofhanoi(n, "A", "C", "B")
