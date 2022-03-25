99fdf2dc7e0b83318c21040fc61ab13fd96cd0ea
Abhishek Maity <abhishek.maity97@gmail.com>
2022-03-25 18:42:16 +0530

    BST traversal updated

--- DSA practice/10-Binary_Search_Tree.py
+++ DSA practice/10-Binary_Search_Tree.py
@@ -37,7 +37,54 @@ class BST:
                 self.insert_helper(this_node.right, key)
 
     def find_inorder_successor(self, this_node):
+        """We want the smallest from the right hand side"""
         myval = this_node
         while myval.left is not None:  # keep on digging the left side of the node
             myval = myval.left
         return myval
+
+    def delete_node(self, this_node, key):
+        if this_node is None:
+            print("Current node is None!!")
+        if key < this_node.key:
+            this_node.left = self.delete_node(this_node.left, key)
+        elif key > this_node.key:
+            this_node.right = self.delete_node(this_node.right, key)
+        else:
+            # Case with no child or 1 child
+            if this_node.left is None:
+                temp = this_node.right
+                this_node = None
+                return temp
+            elif this_node.right is None:
+                temp = this_node.left
+                this_node = None
+                return temp
+
+            # Case with 2 children
+            temp = self.find_inorder_successor(this_node.right)
+
+            this_node.key = temp.key
+            this_node.right = self.delete_node(this_node.right, temp.key)
+
+        return this_node
+
+    # Traversing the Inorder, Preorder & Postorder of the Tree.
+    def inorder(self, this_node):
+        if this_node:
+            self.inorder(this_node.left)
+            print(this_node.key, ', ', end=" ")
+            self.inorder(this_node.right)
+
+    def preorder(self, this_node):
+        if this_node:
+            print(this_node.key, ", ", end=" ")
+            self.preorder(this_node.left)
+            self.preorder(this_node.right)
+
+    def postorder(self, this_node):
+        if this_node:
+            self.postorder(this_node.left)
+            self.postorder(this_node.right)
+            print(this_node.key, ", ", end=" ")
+
